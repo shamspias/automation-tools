726ce0016545f1e3e5812277e7aea18c4571aaee
shamspias <shamsuddinpias0@gmail.com>
2022-09-11 23:41:05 +0600

    add swagger schema for API documentation

--- automation_tools/urls.py
+++ automation_tools/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Automation Tools API",
+        default_version='v1',
+        description="API for Automation Tools",
+        terms_of_service="https://github.com/shamspias/Automation-Tools",
+        contact=openapi.Contact(email="shamsuddinpias0@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    # permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
 ]
