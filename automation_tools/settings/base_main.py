3f536da14546345ef3940fff74d7b46223cac93a
shamspias <shamsuddinpias0@gmail.com>
2022-09-11 23:40:36 +0600

    add rest api setting

--- automation_tools/settings/base_main.py
+++ automation_tools/settings/base_main.py
@@ -119,6 +119,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+
+}
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
