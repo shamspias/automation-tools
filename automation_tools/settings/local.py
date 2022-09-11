5d1bb0f836c6e5b401c15bb3752dc2d9362b51c8
shamspias <shamsuddinpias0@gmail.com>
2022-09-11 22:52:12 +0600

    add automation tools

---
+++ automation_tools/settings/local.py
@@ -0,0 +1,29 @@
+from .base_main import *
+
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static_local"),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
+
+DATABASES = {
+    'default': {
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USERNAME'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
---
+++ automation_tools/settings/stage.py
@@ -0,0 +1,28 @@
+from .base_main import *
+
+STATIC_ROOT = '/home/ubuntu/automation_tools/public_html/static'
+MEDIA_ROOT = '/home/ubuntu/automation_tools/public_html/media'
+
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+
+DATABASES = {
+    'default': {
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USERNAME'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_QUERYSTRING_AUTH = env('AWS_QUERYSTRING_AUTH')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
