1909f94d01b6203a649bc1d56c059ba8dfaf20a8
shamspias <shamsuddinpias0@gmail.com>
2022-09-13 23:06:22 +0600

    ai-blog

--- auto_blog_writer/urls.py
+++ auto_blog_writer/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import BlogWriterAPIView
+from .views import (
+    TopicAPIView,
+    SectionsAPIView,
+    SectionDetailsAPIView
+)
 
 urlpatterns = [
-    path("", BlogWriterAPIView.as_view(), name="blog-writer")
+    path("", TopicAPIView.as_view(), name="blog-writer-topic"),
+    path("sections/", SectionsAPIView.as_view(), name="blog-writer-sections"),
+    path("sections/details", SectionDetailsAPIView.as_view(), name="blog-writer-extend-sections"),
 ]
--- auto_blog_writer/views.py
+++ auto_blog_writer/views.py
@@ -1,8 +1,22 @@
 from rest_framework.views import APIView
 
 
-class BlogWriterAPIView(APIView):
+class TopicAPIView(APIView):
     """
-    API View to create blog
+    API View to get blog topic names
+    """
+    pass
+
+
+class SectionsAPIView(APIView):
+    """
+    API View to get blog section names from topic
+    """
+    pass
+
+
+class SectionDetailsAPIView(APIView):
+    """
+    API View to get blog section details from section with topic
     """
     pass
