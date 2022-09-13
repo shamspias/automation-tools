0c6e0c7bbd9efccbebec399833c07daf12cecca8
shamspias <shamsuddinpias0@gmail.com>
2022-09-14 01:03:51 +0600

    fixed allow host

--- automation_tools/settings/production.py
+++ automation_tools/settings/production.py
@@ -4,7 +4,7 @@ STATIC_ROOT = '/home/ubuntu/automation_tools/public_html/static'
 MEDIA_ROOT = '/home/ubuntu/automation_tools/public_html/media'
 
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
 
 DATABASES = {
     'default': {
