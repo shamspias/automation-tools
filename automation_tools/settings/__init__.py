62119cc2fc9cb6467dfca93de3863cbc301ebcd7
shamspias <shamsuddinpias0@gmail.com>
2022-09-14 00:40:15 +0600

    set stage

--- automation_tools/settings/__init__.py
+++ automation_tools/settings/__init__.py
@@ -1,7 +1,7 @@
 """For production, we'll automatically generate settings from prod.py via ci/cd script"""
 
 # DEV = False
-env_name = "local"
+env_name = "stage"
 
 if env_name == "prod":
     from .production import *
