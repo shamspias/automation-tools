6d81a34ef9ba93c28a828e6cd69680e0a9c40ca7
shamspias <shamsuddinpias0@gmail.com>
2022-09-15 17:17:54 +0600

    url translate

--- document_translator/urls.py
+++ document_translator/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from .views import (
+    TranslateDocumentAPIView,
+    TranslatePDFAPIView,
+    TranslateAudioAPIView,
+    TranslateVideoAPIView
+)
 
 urlpatterns = [
-
+    path('pdf/', TranslatePDFAPIView.as_view(), name='translate-pdf'),
+    path('docx/', TranslateDocumentAPIView.as_view(), name='translate-docx'),
+    path('audio/', TranslateVideoAPIView.as_view(), name='translate-audio'),
+    path('video/', TranslateVideoAPIView.as_view(), name='translate-video'),
 ]
