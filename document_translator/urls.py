65d61fcf69105f0dc2321ddcf55cd3a1e60031da
shamspias <shamsuddinpias0@gmail.com>
2022-09-15 17:18:33 +0600

    url translate audio

--- document_translator/urls.py
+++ document_translator/urls.py
@@ -9,6 +9,6 @@ from .views import (
 urlpatterns = [
     path('pdf/', TranslatePDFAPIView.as_view(), name='translate-pdf'),
     path('docx/', TranslateDocumentAPIView.as_view(), name='translate-docx'),
-    path('audio/', TranslateVideoAPIView.as_view(), name='translate-audio'),
+    path('audio/', TranslateAudioAPIView.as_view(), name='translate-audio'),
     path('video/', TranslateVideoAPIView.as_view(), name='translate-video'),
 ]
