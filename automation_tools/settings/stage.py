5cafb1c66907e35ee5960f8da2473d97e83e5af9
shamspias <shamsuddinpias0@gmail.com>
2022-09-19 13:32:55 +0600

    update static url pdf

--- automation_tools/settings/stage.py
+++ automation_tools/settings/stage.py
@@ -2,7 +2,7 @@ from .base_main import *
 
 STATIC_ROOT = '/home/ubuntu/automation_tools/public_html/static'
 MEDIA_ROOT = '/home/ubuntu/automation_tools/public_html/media'
-
+CONVERTED_FILE_LOCATION = "/home/ubuntu/automation_tools/public_html/media/"
 DEBUG = env('DEBUG')
 ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
 
