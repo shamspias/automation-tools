50646131f9b54ed3acaa75212a4748eab04bc765
shamspias <shamsuddinpias0@gmail.com>
2022-09-24 22:25:10 +0600

    add research paper

---
+++ ai_nlp_tools/urls/educational/__init__.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("/research-paper", include("ai_nlp_tools.urls.educational.research_paper"), name="research-paper-writer"),
+    path("/", include("ai_nlp_tools.urls.educational.")),
+    path("/", include("ai_nlp_tools.urls.educational.")),
+    path("/", include("ai_nlp_tools.urls.educational.")),
+
+]
