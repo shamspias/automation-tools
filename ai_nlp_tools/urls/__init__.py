b99ad69dc2d8b41728c6d9678f7e899c2e2d011e
shamspias <shamsuddinpias0@gmail.com>
2022-09-24 21:38:50 +0600

    add nlp tools

---
+++ ai_nlp_tools/urls/__init__.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("education", include("ai_nlp_tools.urls.educational")),
+    path("life-hack", include("ai_nlp_tools.urls.life_hack")),
+    path("programming", include("ai_nlp_tools.urls.programming")),
+    path("marketing", include("ai_nlp_tools.urls.marketing")),
+]
---
+++ ai_nlp_tools/urls/life_hack/__init__.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("/song", include("ai_nlp_tools.urls.life_hack.song")),
+    path("/", include("ai_nlp_tools.urls.life_hack.")),
+    path("/", include("ai_nlp_tools.urls.life_hack.")),
+    path("/", include("ai_nlp_tools.urls.life_hack.")),
+
+]
