83d2e4259672a6333e2818228d587fedf8ee083f
shamspias <shamsuddinpias0@gmail.com>
2022-09-24 17:18:59 +0600

    manipulation blog topic response

--- auto_blog_writer/utils.py
+++ auto_blog_writer/utils.py
@@ -8,19 +8,17 @@ def generate_blog_topics(prompt):
     context = {}
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt="Generate blog topics on: {}. \n\n".format(prompt),
+        prompt="Generate blog topics on: {}. \n\n 1.".format(prompt),
         temperature=0.7,
-        max_tokens=500,
+        max_tokens=100,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
 
-    context['input'] = prompt
-    my_text = response['choices'][0]['text']
-    context['topics'] = my_text.split("\n")
-    while "" in context['topics']:
-        context['topics'].remove("")
+    my_text = response['choices'][0]['text'].split("\n")
+    context['data'] = [word_value[3:] if word_value[0] != " " else word_value[4:] if i != 0 else word_value[1:] for
+                       i, word_value in enumerate(my_text) if word_value != ""]
     return context
 
 
@@ -30,7 +28,7 @@ def generate_blog_sections(prompt):
         engine="text-davinci-002",
         prompt="Expand the blog title in to high level blog sections: {} \n\n- Introduction: ".format(prompt),
         temperature=0.6,
-        max_tokens=1024,
+        max_tokens=300,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
