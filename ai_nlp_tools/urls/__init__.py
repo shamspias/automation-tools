01e891fb9cc3dcfd4549b987722653fe8d4150ec
shamspias <shamsuddinpias0@gmail.com>
2022-09-24 22:55:53 +0600

    fixed url problems

--- ai_nlp_tools/urls/__init__.py
+++ ai_nlp_tools/urls/__init__.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("education/", include("ai_nlp_tools.urls.educational")),
-    path("life-hack/", include("ai_nlp_tools.urls.life_hack")),
-    path("programming/", include("ai_nlp_tools.urls.programming")),
-    path("marketing/", include("ai_nlp_tools.urls.marketing")),
+    # path("life-hack/", include("ai_nlp_tools.urls.life_hack")),
+    # path("programming/", include("ai_nlp_tools.urls.programming")),
+    # path("marketing/", include("ai_nlp_tools.urls.marketing")),
 ]
